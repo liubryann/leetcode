e6ce551a7369e4929e2a211bcceacd0bc22a9ead
Bryan Liu <liubryan.developer@gmail.com>
2019-09-17 23:52:35 -0400

    938

---
+++ 938.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+// Question:
+// Easy
+// Given the root node of a binary search tree, return the sum of values of all nodes with value between L and R (inclusive).
+
+// The binary search tree is guaranteed to have unique values.
+
+struct TreeNode
+{
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
+};
+
+int main()
+{
+    return 0;
+}
+
+int rangeSumBST(struct TreeNode *root, int L, int R)
+{
+    int sum;
+    return sum;
+}
